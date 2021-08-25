a973d80db9acc6c093d6626b7323f2b44d4fe620
Matt Hanlon <matthanlon@gmail.com>
2021-08-25 14:30:34 -0700

    eliminate multiprocessing and improve efficiency
    - read brain from either yaml or plain text
    - write learned corpus by appending to a plain text file
    - use generators where possible to avoid big reads
    - don't write new phrases if we don't learn from them
    - get rid of <START> and <STOP> tokens, they can be implicit in the corpus
    
    This should make us performant enough we can forgo multiprocessing and do everything in-process

--- markov.py
+++ markov.py
@@ -1,105 +1,140 @@
-import yaml
 import random
-from multiprocess import Lock, Manager, Process
+import yaml
+from itertools import chain, groupby
 
-START = "<START>"
-STOP = "<STOP>"
+START_TOK = "<START>"
+STOP_TOK = "<STOP>"
+
+STOP = object()
+START = object()
 
 # instantiate a Markov object with the source file
 class Markov:
-    def __init__(self, brain_file: str, ignore_words, skip_mp=False):
-        self.brain_file = brain_file
-        self.ignore_words = ignore_words
-        self.skip_mp = skip_mp
-        if self.skip_mp:
-            self.words = list(self.load_corpus(brain_file))
-        else:
-            self.manager = Manager()
-            self.words = self.manager.list(self.load_corpus(brain_file))
-        self.update_cache()
+    def __init__(self, input_file: str, output_file: str, ignore_words):
+        if input_file == output_file:
+            raise ValueError("input and output files must be different")
 
-    @classmethod
-    def load_corpus(cls, source_file: str):
+        self.ignore_words = set(w.upper() for w in ignore_words)
+        self.output_file = output_file
+        self.update_graph_and_corpus(self.corpus_iter(input_file), init=True)
+
+    def corpus_iter(self, source_file: str):
+        """
+        Emit the contents of the source_file as an iterable of token sequences
+        """
         with open(source_file, 'r') as infile:
-            return yaml.load(infile.read(), Loader=yaml.Loader)
+            # this is dumb
+            if source_file.endswith(".yml") or source_file.endswith(".yaml"):
+                words = yaml.load(infile.read(), Loader=yaml.Loader)
+                for is_delim, phrase in groupby(words, lambda w: w in (START_TOK, STOP_TOK)):
+                    if not is_delim:
+                        yield list(phrase)
+            else:
+                for line in infile:
+                    yield from self.tokenize(line)
+
 
     @classmethod
-    def triples(cls, words):
-        if len(words) < 3:
+    def triples_and_stop(cls, words):
+        """
+        Emit 3-grams from the sequence of words, the last one ending with the
+        special STOP token
+        """
+        words = chain(words, [STOP])
+        try:
+            w1 = next(words)
+            w2 = next(words)
+            w3 = next(words)
+            while True:
+                yield (w1, w2, w3)
+                w1, w2, w3 = w2, w3, next(words)
+        except StopIteration:
             return
-        for i in range(len(words) - 2):
-            yield (words[i], words[i+1], words[i+2])
 
     def _ignore(self, word: str):
-        word.strip("\'\"!@#$%^&*().,/\\+=<>?:;").upper() in self.ignore_words
+        return word.strip("\'\"!@#$%^&*().,/\\+=<>?:;").upper() in self.ignore_words
 
     def tokenize(self, sentence: str):
-        words = [w for w in sentence.split() if not self._ignore(w)]
-        if not words:
-            return
+        """
+        Emit a sequence of token lists from the string, ignoring ignore_words.
+        A word ending in certain puntuation ends a given token sequence.
+        """
+        cur = []
+        for w in sentence.split():
+            if self._ignore(w):
+                pass
 
-        yield START
-        for w in words:
-            if any(c in w for c in ('.', '?', '!')):
-                yield STOP
-                yield w.strip(".?!")
-                yield START
+            elif any(w.endswith(c) for c in ('.', '?', '!')):
+                w = w.strip(".?!")
+                if w:
+                    cur.append(w)
+                yield(cur)
+                cur = []
             else:
-                yield w
-        yield STOP
+                cur.append(w)
+        if cur:
+            yield cur
 
-    def update_cache(self, new_sentence = None): 
-        if new_sentence:
-            db = self.cache
-            words = list(self.tokenize(new_sentence))
-        else:
-            db = {START: []}
-            words = self.words
+    def _update_graph_and_emit_changes(self, token_seqs, init=False):
+        """
+        self.graph stores the graph of n-gram trasitions.
+        The keys are single tokens or pairs and the values possible next words in the n-gram.
+        Initial tokens are also specially added to the list at the key START.
 
-        start_of_chain = True
-        for w1, w2, w3 in self.triples(words):
-            if w1 in (START, STOP) or w2 in (START, STOP) or w3 == START:
-                start_of_chain = True
-            else:
-                if start_of_chain:
-                    if w1 not in db[START]:
-                        db[START].append(w1)
-                    next_words = db.setdefault(w1, [])
+        _update_graph_and_emit_changes returns a generator that when run will
+        update the graph with the ngrams taken from each element of token_seqs.
+
+        Yields the token sequence that result in updates so they can be further
+        acted on.
+
+        if init is True reinitialize from an empty graph
+        """
+        if init:
+            self.graph = {START: []}
+
+        for seq in token_seqs:
+            first = True
+            learned = False
+            for w1, w2, w3 in self.triples_and_stop(seq):
+                if first:
+                    if w1 not in self.graph[START]:
+                        self.graph[START].append(w1)
+                        learned = True
+                    next_words = self.graph.setdefault(w1, [])
                     if w2 not in next_words:
                         next_words.append(w2)
-                    start_of_chain = False
-                next_words = db.setdefault((w1, w2), [])
+                        learned = True
+                    first = False
+                next_words = self.graph.setdefault((w1, w2), [])
                 if w3 not in next_words:
                     next_words.append(w3)
-        self.cache = db
+                    learned = True
+            if learned:
+                yield seq
 
-    def update_corpus(self, sentence: str):
-        new_words = list(self.tokenize(sentence))
-        if not new_words:
-            return
-        self.words += new_words
-        lk = None
-        if not self.skip_mp:
-            lk = Lock()
-        with open(self.brain_file, 'w') as outfile:
-            if not self.skip_mp:
-                lk.acquire()
-            outfile.write(yaml.dump(list(self.words), default_flow_style=True))
-            if not self.skip_mp:
-                lk.release()
+    def update_graph_and_corpus(self, token_seqs, init=False):
+        changes = self._update_graph_and_emit_changes(token_seqs, init=init)
+        self.update_corpus(changes, init=init)
+
+    def update_corpus(self, token_seqs, init=False):
+        mode = 'w' if init else 'a'
+        with open(self.output_file, mode) as f:
+            for seq in token_seqs:
+                f.write(" ".join(seq))
+                f.write("\n")
 
     def generate_markov_text(self, seed=None):
-        if seed:
+        if seed and seed in self.graph:
             w1 = seed
         else:
-            w1 = random.choice(self.cache[START])
-        w2 = random.choice(self.cache[w1])
+            w1 = random.choice(self.graph[START])
+        w2 = random.choice(self.graph[w1])
 
         gen_words = [w1]
         while True:
             if w2 == STOP:
                 break
-            w1, w2 = w2, random.choice(self.cache[(w1, w2)])
+            w1, w2 = w2, random.choice(self.graph[(w1, w2)])
             gen_words.append(w1)
 
         message = ' '.join(gen_words)
@@ -108,13 +143,9 @@ class Markov:
     def create_response(self, prompt="", learn=False):
         # set seedword from somewhere in words if there's no prompt
         prompt_tokens = prompt.split()
-        valid_seeds = [tok for tok in prompt_tokens[:-2] if tok in self.cache and tok != START]
+        valid_seeds = [tok for tok in prompt_tokens[:-2] if tok in self.graph]
         seed_word = random.choice(valid_seeds) if valid_seeds else None
         response = self.generate_markov_text(seed_word)
         if learn:
-            if self.skip_mp:
-                self.update_corpus(prompt)
-            else:
-                Process(target=self.update_corpus, args=(prompt,)).start()
-            self.update_cache(prompt)
+            self.update_graph_and_corpus(self.tokenize(prompt))
         return response
